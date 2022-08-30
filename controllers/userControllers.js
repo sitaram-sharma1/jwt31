ead8c94e601abeff2ae906e4979e99ff5e0a914a
sitaram-sharma1 <sharma2017sr@gmail.com>
2022-08-30 16:52:02 +0530

    user reg done

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,5 +1,7 @@
 import UserModel from "../models/User.js";
 import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken";
+
 class UserControllers 
 {                   // _____________________________________
                     // |       NEW USER REGISTRATION       |
@@ -11,14 +13,22 @@ class UserControllers
             const {name,email,password,tc} = await req.body
             if(name && email && password && tc)
             {
-                const isEmailRegistered =await UserModel.find({email:email})
-                if(isEmailRegistered !== null) 
+                const isEmailRegistered = await UserModel.findOne({email:email})
+                if(isEmailRegistered === null) 
                 {
                     const salt =await bcrypt.genSalt(10);
-                    console.log("here");
-                    const hashedPassword =await bcrypt.hash(password,salt);
-                    console.log(hashedPassword);
-                    await res.send("registr")            
+                    const hashedPassword =await bcrypt.hash(password,salt);   
+                    const newUser = new UserModel({
+                                                    name:name,
+                                                    email:email,
+                                                    password:hashedPassword,
+                                                    tc:tc
+                                                    })
+                    await newUser.save();
+                    const token =await jwt.sign({"userID":newUser._id},process.env.JWT_SECRET_KEY,{expiresIn:"2d"})
+                    await UserModel.findByIdAndUpdate(newUser._id , {$set :{"token":token}});
+                    console.log(token);
+                    res.status(201).send({"status":"Success","message":"Registered Successfully"});
                 }
                 else
                 {
@@ -29,11 +39,24 @@ class UserControllers
             {
                 res.status(400).send({"status":"failed","message":"All fields are required"});
             }
-
         } 
         catch (error) 
         {
             res.status(400).send({"status":"failed","message":"Registration failed ! Try again"})
+        }
+    }                   // _____________________________________
+                        // |            USER LOGIN             |
+                        // |___________________________________|
+    static userLogin = async (req,res) =>
+    {
+        try
+        {
+            const { email , password } = req.body
+            res.status(200).send({"status":"Success","message":"Logged-in Successfully"});
+        }
+        catch (error) 
+        {
+            res.status(400).send({"status":"failed","message":"try again to Log-in "})
         }
     }
 }
